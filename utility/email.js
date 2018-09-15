158403aed87e69d842f78a6d3e76a5d000d3a9b9
muntasir2165 <muntasir2165@hotmail.com>
2018-09-15 12:03:55 -0400

    added sms/text message functionality

--- utility/email.js
+++ utility/email.js
@@ -26,7 +26,7 @@ module.exports = function(emailTo, emailSubject, isEmailBodyHtml, emailBody) {
     }
     transporter.sendMail(mailOptions, function(error, info){
         if (error) {
-          console.log("Email delivery failed.\n" + error);
+          console.log("Email delivery failed.\n" + JSON.stingify(error));
       } else {
             // info object example:
             // {"accepted":["muntasir2165@hotmail.com"],"rejected":[],"envelopeTime":141,"messageTime":400,"messageSize":364,"response":"250 2.0.0 OK 1536972559 80-v6sm266115itk.14 - gsmtp","envelope":{"from":"student.e.store.no.reply@gmail.com","to":["muntasir2165@hotmail.com"]},"messageId":"<e187370c-86ad-33e1-8f6e-d7b54e4c0eaf@gmail.com>"}  
